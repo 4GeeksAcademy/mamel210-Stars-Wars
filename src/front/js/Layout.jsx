9ce5de5d36a2a785acc461e7805546f63dee3ea9
mamel210 <mamel210@gmail.com>
2024-09-06 22:58:11 +0000

    contact-list

--- src/front/js/Layout.jsx
+++ src/front/js/Layout.jsx
@@ -6,10 +6,12 @@ import ScrollToTop from "./component/ScrollToTop.jsx";
 import { BackendURL } from "./component/BackendURL.jsx";
 import { Footer } from "./component/Footer.jsx";
 import { Navbar } from "./component/Navbar.jsx";
-//vCustom Pagues of Views
+// Custom Pagues of Views
 import { Single } from "./pages/Single.jsx";
 import { Home } from "./pages/Home.jsx";
 import { Demo } from "./pages/Demo.jsx";
+import { AddContact } from './pages/AddContact.jsx';
+import { EditContact } from './pages/EditContact.jsx';
 
 
 // Create your first Component
@@ -17,7 +19,7 @@ const Layout = () => {
     // The basename is used when your project is published in a subdirectory and not in the root of the domain
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
+    if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL />;
 
     return (
         <div>
@@ -26,14 +28,16 @@ const Layout = () => {
                     <Navbar />
                     <Routes>
                         <Route element={<Home />} path="/" />
+                        <Route element={<AddContact />} path='/add-contact' />
+                        <Route element={<EditContact />} path='/edit-contact' />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
-                        <Route element={<h1>Not found!</h1>} />
+                        <Route element={<h1>Not found!</h1>} path="*" />
                     </Routes>
                     <Footer />
                 </ScrollToTop>
-            </BrowserRouter>
-        </div>
+            </BrowserRouter >
+        </div >
     );
 };
 
